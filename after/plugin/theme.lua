1d7c893d3df99412305e02c36d65a7e241bb108d
Daniel Del Balso <d2b@ansatz-ma.local>
2025-02-25 11:02:54 +0100

    kanagawa, like the good old days

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -31,36 +31,38 @@
 -- vim.o.background = "dark"
 -- vim.cmd("colorscheme gruvbox")
 
--- require("kanagawa").setup({
--- 	compile = false, -- enable compiling the colorscheme
--- 	undercurl = true, -- enable undercurls
--- 	commentStyle = { italic = true },
--- 	functionStyle = {},
--- 	keywordStyle = { italic = true },
--- 	statementStyle = { bold = true },
--- 	typeStyle = {},
--- 	transparent = false, -- do not set background color
--- 	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
--- 	terminalColors = true, -- define vim.g.terminal_color_{0,17}
--- 	colors = { -- add/modify theme and palette colors
--- 		palette = {},
--- 		theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
--- 	},
--- 	overrides = function() -- add/modify highlights
--- 		return {}
--- 	end,
--- 	theme = "wave", -- Load "wave" theme when 'background' option is not set
--- 	background = { -- map the value of 'background' option to a theme
--- 		dark = "wave", -- try "wave" !
--- 		light = "lotus",
--- 	},
--- })
--- vim.cmd("colorscheme kanagawa")
+require("kanagawa").setup({
+	compile = false, -- enable compiling the colorscheme
+	undercurl = true, -- enable undercurls
+	commentStyle = { italic = true },
+	functionStyle = {},
+	keywordStyle = { italic = true },
+	statementStyle = { bold = true },
+	typeStyle = {},
+	transparent = false, -- do not set background color
+	dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+	terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	colors = { -- add/modify theme and palette colors
+		palette = {},
+		theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+	},
+	overrides = function() -- add/modify highlights
+		return {}
+	end,
+	theme = "wave", -- Load "wave" theme when 'background' option is not set
+	background = { -- map the value of 'background' option to a theme
+		dark = "wave", -- try "wave" !
+		light = "lotus",
+	},
+})
+vim.cmd("colorscheme kanagawa")
 
-require("ibl").setup()
-require("flexoki").setup()
+require("ibl").setup({
+	indent = {
+		char = "┊",
+	},
+})
 
-vim.cmd("colorscheme flexoki")
 require("lualine").setup({
 	options = {
 		component_separators = "",
@@ -83,10 +85,13 @@ require("lualine").setup({
 
 require("nvim-tree").setup({
 	sort_by = "case_sensitive",
+	update_focused_file = {
+		enable = true,
+	},
 	view = {
 		width = 30,
 	},
 	filters = {
-		dotfiles = false,
+		dotfiles = true,
 	},
 })
