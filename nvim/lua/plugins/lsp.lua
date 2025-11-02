f073f83716e85f431d233bef3f74c8ea28a63275
Daniel Del Balso <daniel.delbalso@gmail.com>
2025-11-02 17:48:10 +0100

    sync

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,8 +2,8 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = { "saghen/blink.cmp" },
-		opts = {
-			servers = {
+        config =  function(_, opts)
+            local servers = {
 				lua_ls = {},
 				ts_ls = {},
 				tailwindcss = {},
@@ -43,19 +43,15 @@ return {
 				},
 				terraformls = {},
 				dockerls = {},
-				helm_ls = {},
-			},
-		},
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
-			for server, config in pairs(opts.servers) do
-				-- passing config.capabilities to blink.cmp merges with the capabilities in your
-				-- `opts[server].capabilities, if you've deeined it
-				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				lspconfig[server].setup(config)
-			end
-		end,
-	},
+				helm_ls = {}
+			}
+
+            for server, config in pairs(servers) do
+                vim.lsp.config(server, config);
+                vim.lsp.enable(server);
+            end
+        end
+    },
 	{
 		"folke/lazydev.nvim",
 		ft = "lua", -- only load on lua files
